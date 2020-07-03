78c03763e4eebabcc6e999e97caf4bf1ff247a8a
Mantova <jf.mantova@gmail.com>
2020-07-03 20:58:56 -0300

    [ADD] insertintoarray.c

--- arrays-strings-and-functions/insert-an-element-in-an-array.c
+++
@@ -1,4 +0,0 @@
-//
-// Created by Joao Filipe Mantova on 7/3/20.
-//
-
---
+++ arrays-strings-and-functions/insertintoarray.c
@@ -0,0 +1,36 @@
+//
+// Created by Joao Filipe Mantova on 7/3/20.
+//
+
+#include <stdio.h>
+
+#define max 100
+
+int main() {
+    int p[max], size, position;
+    printf("Enter the length of the array: ");
+    scanf("%d", &size);
+
+    printf("Print %d elements of the array:\n", size);
+    for (int i = 0; i < size; i++)
+        scanf("%d", &p[i]);
+
+    printf("\nThe array is:\n");
+    for (int i = 0; i < size; i++)
+        printf("%d\n", p[i]);
+
+    printf("\nEnter position where to insert: ");
+    scanf("%d", &position);
+    position--;
+    for (int i = size - 1; i >= position; i--)
+        p[i + 1] = p[i];
+
+    printf("\nEnter value to insert: ");
+    scanf("%d", &p[position]);
+
+    printf("\nArray after insertion of element:\n");
+    for (int i = 0; i <= size; i++)
+        printf("\n%d", p[i]);
+
+    return 0;
+}
