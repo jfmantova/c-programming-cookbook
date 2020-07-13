5bea1fb4cc75a4a1926ca5078c3ae7eaf554f288
Mantova <jf.mantova@gmail.com>
2020-07-12 21:46:23 -0300

    [ADD] countofeach.c

---
+++ arrays-strings-and-functions/countofeach.c
@@ -0,0 +1,51 @@
+//
+// Created by Joao Filipe Mantova on 7/12/20.
+//
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    const int charSize = 80;
+    char str[charSize], chr[charSize];
+
+    printf("Enter a word: ");
+    scanf("%s", str);
+
+    const int wordLength = strlen(str);
+
+    int index = 0, counter[80];
+
+    for (int i = 0; i < wordLength; ++i) {
+        if (i == 0) {
+            chr[i] = str[i];
+            counter[i] = 1;
+            index++;
+            continue;
+        }
+
+        char letter = str[i];
+        int sameLetterIndex = -1;
+        for (int j = 0; j <= index; ++j) {
+            if (chr[j] == letter) {
+                sameLetterIndex = j;
+                break;
+            }
+        }
+
+        if (sameLetterIndex >= 0) {
+            counter[sameLetterIndex] = ++counter[sameLetterIndex];
+        } else {
+            chr[index] = letter;
+            counter[index] = 1;
+            index++;
+        }
+    }
+
+    chr[index + 1] = '\0';
+
+    for (int i = 0; i < index; ++i) {
+        printf("%c - %d\n", chr[i], counter[i]);
+    }
+
+    return 0;
+}
