c003c9b9a81e2220b6e5f9c19a9cb62857826f22
Mantova <jf.mantova@gmail.com>
2020-07-19 21:10:30 -0300

    [ADD] pointertostruct.c

---
+++ chapter-4/pointertostruct.c
@@ -0,0 +1,30 @@
+//
+// Created by Joao Filipe Mantova on 7/19/20.
+//
+
+#include <stdio.h>
+
+struct Cart {
+    int orderNumber;
+    char emailAddress[30];
+    char password[30];
+};
+
+void main() {
+    struct Cart myCart, *pointer1, *pointer2;
+    printf("%lu bytes\n", sizeof(myCart.emailAddress));
+    pointer1 = &myCart;
+
+    printf("Enter order number: ");
+    scanf("%d", &myCart.orderNumber);
+    printf("Enter email address: ");
+    scanf("%s", myCart.emailAddress);
+    printf("%lu bytes\n", sizeof(myCart.emailAddress));
+    printf("Enter password: ");
+    scanf("%s", myCart.password);
+
+    printf("\nDetails: \n");
+    printf("Order number: %d\n", pointer1->orderNumber);
+    printf("Email: %s\n", pointer1->emailAddress);
+    printf("Password: %s\n", pointer1->password);
+}
